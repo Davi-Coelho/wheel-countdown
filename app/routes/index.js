a6926aa55ff583f370cdd368994a04952c4cad0c
Davi Coelho <davi.cmbarreto@gmail.com>
2022-07-10 23:31:46 -0400

    feat: cors adicionado;

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
+const cors = require('cors')
+
 module.exports = (application) => {
     application.get('/wheel-countdown', (req, res) => {
         application.app.controllers.index.index(application, req, res)
     })
 
-    application.post('/wheel-countdown', (req, res) => {
+    application.post('/wheel-countdown', cors({ origin: 'https://streamlabs.com/', optionsSuccessStatus: 200 }), (req, res) => {
         application.app.controllers.index.wheelCounter(application, req, res)
     })
 }
--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const consign = require('consign')
+const cors = require('cors')
 const app = express()
 
 app.set('view engine', 'ejs')
