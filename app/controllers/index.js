7444c088740208ffe20a55815172a7b676220768
Davi Coelho <davi.cmbarreto@gmail.com>
2022-07-28 23:40:17 -0400

    fix: casos de não ser um comando;

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,22 +2,29 @@ module.exports.wheelCounter = (application, req, res) => {
     const data = req.query.msg
     const channel = req.query.channel
     const command = data.split(' ')
-    let value = parseFloat(command[1])
     console.log(data)
 
-    switch (command[2].toLowerCase()) {
-        case 'h':
-            value *= 1000 * 60 * 60
-            break
-        case 'min':
-            value *= 1000 * 60
-            break
+    if (command.length === 3) {
+        let value = parseFloat(command[1])
+        let isCommand = true
+    
+        switch (command[2].toLowerCase()) {
+            case 'h':
+                value *= 1000 * 60 * 60
+                break
+            case 'min':
+                value *= 1000 * 60
+                break
+            default: 
+                isCommand = false
+                break
+        }
+
+        if (isCommand) {
+            value = command[0].toLowerCase() === 'adicionar' ? value : value * (-1)
+            const ws = Array.from(application.appWs.clients).filter(el => el.id === channel)
+            ws.forEach(el => el.send(value))
+        }
     }
-
-    value = command[0].toLowerCase() === 'adicionar' ? value : value * (-1)
-
-    const ws = Array.from(application.appWs.clients).filter(el => el.id === channel)
-
-    ws.forEach(el => el.send(value))
     res.sendStatus(200)
 }
